4dde37349a31903d1adf7fc05175d55e514b5e67
mitalijena-87 <mitali.p.jena@gmail.com>
2024-11-19 21:21:26 +0530

    Reading contents, reverse and write into a new file changes

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -2,17 +2,18 @@ package org.example;
 
 import org.example.service.FileProcessorService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
 import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.nio.file.Path;
 
 import static org.mockito.ArgumentMatchers.anyString;
 
 import static org.mockito.Mockito.*;
-import static org.powermock.api.mockito.PowerMockito.whenNew;
 
-//@ExtendWith(PowerMockExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class MainTest {
 
     @InjectMocks
@@ -27,9 +28,21 @@ public class MainTest {
 
         FileProcessorService fileProcessorServiceMock = mock(FileProcessorService.class);
         Main main = new Main(fileProcessorServiceMock);
-        whenNew(FileProcessorService.class).withNoArguments().thenReturn(fileProcessorServiceMock);
 
         when(fileProcessorServiceMock.readAndReverseContent(anyString())).thenReturn("RQP");
+        when(fileProcessorServiceMock.writeContent(anyString(),anyString())).thenReturn("");
+
+        main.fileContentProcessing();
+    }
+
+    @Test
+    public void testMainMethodWithNullContent() throws Exception {
+        String[] args = new String[] {};
+
+        FileProcessorService fileProcessorServiceMock = mock(FileProcessorService.class);
+        Main main = new Main(fileProcessorServiceMock);
+
+        when(fileProcessorServiceMock.readAndReverseContent(anyString())).thenReturn(null);
         when(fileProcessorServiceMock.writeContent(anyString(),anyString())).thenReturn("");
 
         main.fileContentProcessing();
